eec8551cd61f1b23e71ab868522a8fc49706eccb
Jason B <jasonsburke90@gmail.com>
2022-04-24 16:52:10 -0500

    more model work

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,11 +1,12 @@
 const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
+
 // schema to create a reaction subdocument
 const reactionSchema = new Schema(
   {
     reactionId: {
-      type: Schema.Types.ObjectIds,
-      default: () => new Types.ObjectIds(),
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
 
     reactionBody: {
